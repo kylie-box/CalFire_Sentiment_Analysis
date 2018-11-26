a1e7a31c53d036588b9375745242cb15413356b4
Jingyi(Kylie) He <kyliehe616@gmail.com>
2018-11-25 23:52:04 -0500

    Add sentiment tag by emojis

--- code/emojitags.py
+++ code/emojitags.py
@@ -8,9 +8,12 @@ import unicodedata
 from unidecode import unidecode
 import re
 
-df = pd.read_csv('fire-2018-11-21_new.csv')
+INPUT_DATA_PATH = './data/data_preprocessed/fire-2018-11-{}_new.csv'
+OUTPUT_DATA_PATH = './data/data_preprocessed/automatic_sentiment_tag_by_emoji/fire-2018-11-{}_new.csv'
+
 
 def deEmojify(inputString):
+    # get all emojis in the form of text in a corpora
     returnString = ""
 
     for character in inputString:
@@ -27,29 +30,52 @@ def deEmojify(inputString):
                     # print(a)
                     returnString += " [" + unicodedata.name(character).lower() + "]"
                 except ValueError:
-                    returnString += " [x]"
+                    pass
 
     return returnString
 
 
-
 emoji_dict = {}
 
-
-with open('emoji_dict.txt',"r") as f:
+with open('emoji_dict.txt', "r") as f:
     for line in f:
         name = deEmojify(line).strip()[:-2].strip()
-        emoji_dict.update({name:
-                           deEmojify(line).strip()[-2:].strip()})
+        # print(name)
+        emoji_dict.update({name:int(deEmojify(line).strip()[-2:].strip())})
 
-for k in list(emoji_dict.keys()):
-    if k.startswith('[x]') or k.startswith('[?]') or k.endswith('[x]') or k.endswith('[?]'):
-        emoji_dict.pop(k)
 
-for row in df['tweet']:
-    row = deEmojify(row)
-    key = row[row.find("[")+1:row.find("]")]
-    print(key)
-# s = deEmojify(s)
-# s = s[s.find("[")+1:s.find("]")]
-# print(s)
+for date in range(12,22):
+
+    file_name = INPUT_DATA_PATH.format(date)
+    df = pd.read_csv(file_name)
+
+    for k in list(emoji_dict.keys()):  # remove all unknown emojis
+        if k.startswith('[?]') or k.endswith('[?]') or k == "" or len(re.findall(r'\[.*?\]', k)) > 1:
+            emoji_dict.pop(k)
+
+    # get sentiment based on the emoji dictionary
+    ct = 0
+    for i, row in enumerate(df['tweet']):
+
+        emojis = deEmojify(row)    # one tweet might have multiple Emojis
+        key = re.findall(r'\[.*?\]', emojis)   # return the list of emojis
+
+        ct = ct + 1
+        emoji_score = 0
+        val = 0
+        try:
+            for k in key:
+                val = emoji_dict.get(k)
+                if val is None:
+                    val = 0
+                emoji_score = emoji_score+int(val)
+        except:
+            continue
+        if emoji_score > 0:
+            df.at[i, 'score'] = 1
+        elif emoji_score < 0:
+            df.at[i, 'score'] = 0
+            # print(emoji_score)
+
+    write_file = OUTPUT_DATA_PATH.format(date)
+    df.to_csv(write_file,index=False)
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 for i in range(12,22):
 
-    df = pd.read_csv('fire-2018-11-{}.csv'.format(i))
+    df = pd.read_csv('./data/data_raw/fire-2018-11-{}.csv'.format(i))
 
     df['tweet'] = df['tweet'].str.replace(r"http\S+", "")
     df['tweet'] = df['tweet'].str.replace(r"\n+", "")
 
 
-    df.to_csv('fire-2018-11-{}_new.csv'.format(i), index=False)
+    df.to_csv('./data/data_preprocessed/fire-2018-11-{}_new.csv'.format(i), index=False)
